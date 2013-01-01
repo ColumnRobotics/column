9c4880875eaf74c1ed7f51dc1a9c10b5cc31c2c9
Erik Sjoberg <sjobeek@gmail.com>
2012-12-31 21:07:17 -0500

    Fix launch scripts for drone #2

--- launch/global_planner.bash
+++ launch/global_planner.bash
@@ -2,17 +2,17 @@
 roscore &
 sleep 1
 # Record rosbags, excluding video
-rosbag record -a -x "/camera/image(.*)|/april_tag_debug/(.*)" &
+#rosbag record -a -x "/camera/image(.*)|/april_tag_debug/(.*)" &
 
-cd ~/catkin_ws/
-source devel/setup.bash
-source ./src/gscam_config.sh
-
-cd ~/column_ws/
-source devel/setup.bash
+# Add GScam ros env and config
+source ~/Development/ROS-semi-official/devel/setup.bash
+source ~/catkin_ws/src/column/gscam_config.sh
 
 rosrun gscam gscam &
 sleep 1
+
+cd ~/catkin_ws/
+source devel/setup.bash
 
 roslaunch mavros px4.launch &
 sleep 1
